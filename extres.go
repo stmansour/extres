dee212e35d19f764c753271b5c1dbdfc1a9ba9fd
Steve Mansour <sman@stevemansour.com>
2021-03-26 11:50:05 -0700

    FIX: ResetPWList an array

--- extres.go
+++ extres.go
@@ -21,57 +21,57 @@ const (
 // ExternalResources is a type defining several resources
 // that are used across the Accord suite.
 type ExternalResources struct {
-	Env            int    `json:"Env"`
-	AuthNHost      string `json:"AuthNHost"`
-	AuthNType      string `json:"AuthNType"`
-	AuthNPort      int    `json:"AuthNPort"`
-	Dbuser         string `json:"Dbuser"`
-	Dbname         string `json:"Dbname"`
-	Dbpass         string `json:"Dbpass"`
-	Dbhost         string `json:"Dbhost"`
-	Dbport         int    `json:"Dbport"`
-	Dbtype         string `json:"Dbtype"`
-	SMTPHost       string `json:"SmtpHost"`
-	SMTPPort       int    `json:"SmtpPort"`
-	SMTPLogin      string `json:"SmtpLogin"`
-	SMTPPass       string `json:"SmtpPass"`
-	RRDbuser       string `json:"RRDbuser"`
-	RRDbname       string `json:"RRDbname"`
-	RRDbpass       string `json:"RRDbpass"`
-	RRDbhost       string `json:"RRDbhost"`
-	RRDbport       int    `json:"RRDbport"`
-	RRDbtype       string `json:"RRDbtype"`
-	MojoDbuser     string `json:"MojoDbuser"`
-	MojoDbname     string `json:"MojoDbname"`
-	MojoDbpass     string `json:"MojoDbpass"`
-	MojoDbhost     string `json:"MojoDbhost"`
-	MojoDbport     int    `json:"MojoDbport"`
-	MojoDbtype     string `json:"MojoDbtype"`
-	MojoWebAddr    string `json:"MojoWebAddr"`
-	WREISDbuser    string `json:"WREISDbuser"`
-	WREISDbname    string `json:"WREISDbname"`
-	WREISDbpass    string `json:"WREISDbpass"`
-	WREISDbhost    string `json:"WREISDbhost"`
-	WREISDbport    int    `json:"WREISDbport"`
-	WREISDbtype    string `json:"WREISDbtype"`
-	Timezone       string `json:"Timezone"`       // see $GOROOT/lib/time/zoneinfo.zip, or try: tar tvf $GOROOT/lib/time/zoneinfo.zip
-	SessionTimeout int    `json:"SessionTimeout"` // session timeout in minutes
-	RootHandler    string `json:"RootHandler"`    // Handler for web app where url filepath is "/"
-	Tester1Name    string `json:"Tester1Name"`    // username for tester 1
-	Tester1Pass    string `json:"Tester1Pass"`    // password for tester 1
-	Tester2Name    string `json:"Tester2Name"`    // username for tester 2
-	Tester2Pass    string `json:"Tester2Pass"`    // password for tester 2
-	RepoUser       string `json:"RepoUser"`       // for Artifactory
-	RepoPass       string `json:"RepoPass"`       // for Artifactory
-	RepoURL        string `json:"RepoURL"`        // for Artifactory
-	S3Region       string `json:"S3Region"`       //
-	S3BucketHost   string `json:"S3BucketHost"`   // S3 Bucket host. It is for storing profile images. e.g., https://s3.ap-south-1.amazonaws.com
-	S3BucketName   string `json:"S3BucketName"`   // S3 Bucket name. e.g., upload-profile-image
-	S3BucketKeyID  string `json:"S3BucketKeyID"`  // Access key id to use AWS S3 service
-	S3BucketKey    string `json:"S3BucketKey"`    // Secret key to use AWS S3 service
-	CryptoKey      string `json:"CryptoKey"`      // key for encryption/decription, must be 32 chars long
-	MapKey         string `json:"MapKey"`         // key for using google map
-	ResetPWList    string `json:"ResetPWList"`    // list of domains for which reset password is valid
+	Env            int      `json:"Env"`
+	AuthNHost      string   `json:"AuthNHost"`
+	AuthNType      string   `json:"AuthNType"`
+	AuthNPort      int      `json:"AuthNPort"`
+	Dbuser         string   `json:"Dbuser"`
+	Dbname         string   `json:"Dbname"`
+	Dbpass         string   `json:"Dbpass"`
+	Dbhost         string   `json:"Dbhost"`
+	Dbport         int      `json:"Dbport"`
+	Dbtype         string   `json:"Dbtype"`
+	SMTPHost       string   `json:"SmtpHost"`
+	SMTPPort       int      `json:"SmtpPort"`
+	SMTPLogin      string   `json:"SmtpLogin"`
+	SMTPPass       string   `json:"SmtpPass"`
+	RRDbuser       string   `json:"RRDbuser"`
+	RRDbname       string   `json:"RRDbname"`
+	RRDbpass       string   `json:"RRDbpass"`
+	RRDbhost       string   `json:"RRDbhost"`
+	RRDbport       int      `json:"RRDbport"`
+	RRDbtype       string   `json:"RRDbtype"`
+	MojoDbuser     string   `json:"MojoDbuser"`
+	MojoDbname     string   `json:"MojoDbname"`
+	MojoDbpass     string   `json:"MojoDbpass"`
+	MojoDbhost     string   `json:"MojoDbhost"`
+	MojoDbport     int      `json:"MojoDbport"`
+	MojoDbtype     string   `json:"MojoDbtype"`
+	MojoWebAddr    string   `json:"MojoWebAddr"`
+	WREISDbuser    string   `json:"WREISDbuser"`
+	WREISDbname    string   `json:"WREISDbname"`
+	WREISDbpass    string   `json:"WREISDbpass"`
+	WREISDbhost    string   `json:"WREISDbhost"`
+	WREISDbport    int      `json:"WREISDbport"`
+	WREISDbtype    string   `json:"WREISDbtype"`
+	Timezone       string   `json:"Timezone"`       // see $GOROOT/lib/time/zoneinfo.zip, or try: tar tvf $GOROOT/lib/time/zoneinfo.zip
+	SessionTimeout int      `json:"SessionTimeout"` // session timeout in minutes
+	RootHandler    string   `json:"RootHandler"`    // Handler for web app where url filepath is "/"
+	Tester1Name    string   `json:"Tester1Name"`    // username for tester 1
+	Tester1Pass    string   `json:"Tester1Pass"`    // password for tester 1
+	Tester2Name    string   `json:"Tester2Name"`    // username for tester 2
+	Tester2Pass    string   `json:"Tester2Pass"`    // password for tester 2
+	RepoUser       string   `json:"RepoUser"`       // for Artifactory
+	RepoPass       string   `json:"RepoPass"`       // for Artifactory
+	RepoURL        string   `json:"RepoURL"`        // for Artifactory
+	S3Region       string   `json:"S3Region"`       //
+	S3BucketHost   string   `json:"S3BucketHost"`   // S3 Bucket host. It is for storing profile images. e.g., https://s3.ap-south-1.amazonaws.com
+	S3BucketName   string   `json:"S3BucketName"`   // S3 Bucket name. e.g., upload-profile-image
+	S3BucketKeyID  string   `json:"S3BucketKeyID"`  // Access key id to use AWS S3 service
+	S3BucketKey    string   `json:"S3BucketKey"`    // Secret key to use AWS S3 service
+	CryptoKey      string   `json:"CryptoKey"`      // key for encryption/decription, must be 32 chars long
+	MapKey         string   `json:"MapKey"`         // key for using google map
+	ResetPWList    []string `json:"ResetPWList"`    // list of domains for which reset password is valid
 }
 
 // ReadConfig will read the configuration file "config.json" if
